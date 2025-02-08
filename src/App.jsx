1d617b5a331b145e882f3c40031ee70f35f5046a
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-08 23:49:39 +0400

    create basic add/delete user list, without styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState } from 'react'
 
 import './App.css'
 import User from './assets/components/User';
@@ -7,15 +7,39 @@ const users = [
     { id: 1, name: "Alice", age: 25 },
     { id: 2, name: "Bob", age: 30 },
     { id: 3, name: "Charlie", age: 28 },
-    { id: 4, name: "David", age: 22 }
   ];
 
 function App() {
+    const [name,setName]=useState("");
+    const [age,setAge]=useState("")
+    const [userData,setUsersData]=useState(users)
+
+    const handleSubmit=(event)=> {
+        event.preventDefault()
+        setUsersData((userData)=> 
+        [...userData,{name,age,id:Math.random()}]);
+        setName(""); // Clear input fields
+        setAge("");
+    }
+        
   return (
     <>
-      {users.map((user)=>(
-        <User key={user.id} user={user}/>
+      {userData.map((user)=>(
+        <User key={user.id} user={user} userData={userData} setUsersData={setUsersData}/>
       ))}
+      <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" value={name} onChange={(event)=>
+         setName(event.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="age">Age</label>
+        <input type="text" value={age} onChange={(event)=>
+         setAge(event.target.value)}  />
+      </div>
+      <button type='submit'>submit</button>
+      </form>
     </>
   )
 }
--- src/assets/components/User.jsx
+++ src/assets/components/User.jsx
@@ -1,10 +1,14 @@
 
 
-export default function User({user}) {
+export default function User({user,userData,setUsersData}) {
+    const handleDlete=()=> {
+       setUsersData(prevUsersData => prevUsersData.filter((item)=>item.id !== user.id))
+    }
     return (
         <div className='user'>
             <h1>{user.name}</h1>
             <p>{user.age}</p>
+            <button onClick={handleDlete} style={{backgroundColor:"red"}}>Delete</button>
         </div>
     )
 }
